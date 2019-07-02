985955f915b4845cba8f3cee0342484e6cb2dc2a
Danita Craft <chantrieri@hotmail.com>
2019-07-02 18:33:22 -0400

    checking files before rearranging/refactoring js

--- vot.js
+++ vot.js
@@ -1,37 +1,83 @@
 
-	/*On click, the JS popup opens and closes
-	function myFunction(myFancyId) {
-    var popups = document.getElementsByClassName("popuptext");
-  for (var i=0;  i<popups.length; i++){ 
-    popups[i].classList.remove("show");   
-  }
-  	var popup = document.getElementById(myFancyId);
-		popup.classList.add("show");
-  } */
 
 //On scroll function
 // window.onscroll = function(){myScrollFunction()};
 const pane = document.getElementById("pane");
 pane.onscroll = function(){myScrollFunction()};
 function myScrollFunction() {
-  console.log("I was called.");
+console.log("I was called.");
+
+
+if(pane.scrollLeft > 125) {
+  document.getElementById("myPopupHalf").classList.add("show");
+}
   
+if(pane.scrollLeft > 272 || pane.scrollLeft < 125) {
+    document.getElementById("myPopupHalf")
+    .classList.remove("show");
+}
+
 if(pane.scrollLeft > 200) {
   document.getElementById("myPopup1").classList.add("show");
 }
 
 if(pane.scrollLeft > 300 || pane.scrollLeft < 200) {
-    document.getElementById("myPopup1").classList.remove("show");
+    document.getElementById("myPopup1")
+    .classList.remove("show");
 }
 
 if(pane.scrollLeft > 300){
-    document.getElementById("myPopup2")
-    .classList.add("show");
-  }
-
-  if(pane.scrollLeft > 400 || pane.scrollLeft < 300) {
-    document.getElementById("myPopup2")
-    .classList.remove("show");
-  }
+  document.getElementById("myPopupHalf2")
+  .classList.add("show");
 }
 
+if(pane.scrollLeft > 380 || pane.scrollLeft < 300) {
+  document.getElementById("myPopupHalf2")
+  .classList.remove("show");
+}
+
+
+
+
+if(pane.scrollLeft > 320){
+    document.getElementById("myPopup2")
+    .classList.add("show");
+}
+
+  if(pane.scrollLeft > 400 || pane.scrollLeft < 320) {
+    document.getElementById("myPopup2")
+    .classList.remove("show");
+ }
+
+ if(pane.scrollLeft > 400) {
+  document.getElementById("myPopupHalf3")
+  .classList.add("show");
+}
+
+if(pane.scrollLeft > 480|| pane.scrollLeft < 400) {
+  document.getElementById("myPopupHalf3")
+  .classList.remove("show"); 
+}
+
+
+if(pane.scrollLeft > 420) {
+  document.getElementById("myPopup3")
+  .classList.add("show");
+}
+
+if(pane.scrollLeft > 500 || pane.scrollLeft < 420) {
+  document.getElementById("myPopup3")
+  .classList.remove("show"); 
+}
+
+
+if(pane.scrollLeft > 500) {
+  document.getElementById("myPopup4")
+  .classList.add("show");
+}
+
+if(pane.scrollLeft > 600 || pane.scrollLeft < 500) {
+  document.getElementById("myPopup4")
+  .classList.remove("show"); 
+  } 
+}
