fdfe83ebd6112f49eb84dd1a47f5285a435fb248
Danita Craft <chantrieri@hotmail.com>
2019-07-02 20:28:24 -0400

    working on functions for scroll-load

--- vot.js
+++ vot.js
@@ -1,3 +1,51 @@
+// Iterative scroll function
+
+const timelineInfo = [
+  {
+    id: "js-day-1-1", 
+    text: "In the beginning, God created the heavens and the earth.", 
+    x1: 125, 
+    x2: 275,
+    y:  30,
+  } ,
+  {
+    id: "js-day-1-9", 
+    text: "Day 2, or some other thing happens.", 
+    x1: 300, 
+    x2: 475,
+    y:  40,
+  }
+];
+
+const displayItem = (id, x1) => {
+  const pane = document.getElementById("pane");
+
+  return (pane.onscroll = function(){
+    console.log(" Hello " + x1)
+    if(pane.scrollLeft > x1) {
+      document.getElementById(id).classList.add("show");
+    }
+  })
+}
+
+function buildTimeline() {
+  const timelineDiv = document.getElementById("timeline");
+  timelineInfo.map(function(item) {
+    // html content to render
+    const htmlContent = `
+    <div class="popup" id="${item.id}" data-x1="${item.x1}" data-x2="${item.x2}" data-y="${item.y}" onload="displayItem('${item.id}', ${item.x1})">
+      <span class="popuptext">${item.text}</span>
+    </div> 
+    `;
+    return timelineDiv.innerHTML += htmlContent;
+  })
+}
+
+
+
+
+ /*
+
 
 
 //On scroll function
@@ -80,4 +128,5 @@ if(pane.scrollLeft > 600 || pane.scrollLeft < 500) {
   document.getElementById("myPopup4")
   .classList.remove("show"); 
   } 
-}
+   
+}*/
